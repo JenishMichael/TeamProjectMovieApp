69b31b02513828adc15271be18602e66626ce50f
Jenish Michael <jenishmichael21@gmail.com>
2025-05-15 11:06:40 +0530

    JenishMichael | initial commit

---
+++ src/test/java/com/java/CapStoneProject/CapStoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.CapStoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapStoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
